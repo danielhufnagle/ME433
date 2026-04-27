09f1b92f9f3d7001b476bf0e682847e0678b4e58
danielhufnagle <114324335+danielhufnagle@users.noreply.github.com>
2026-04-27 08:25:02 -0500

    added heartbeat

--- HW3_I2C_Expander/HW3_I2C_Expander.c
+++ HW3_I2C_Expander/HW3_I2C_Expander.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "MCP23008.h"
 
 #define I2C_SDA 8
 #define I2C_SCL 9
+#define HEARTBEAT_PIN 0
+#define HEARTBEAT_INTERVAL 500000
+
+int prev_time;
+int heartbeat_state = 0;
 
 int main()
 {
     stdio_init_all();
+    gpio_init(HEARTBEAT_PIN);
+    gpio_set_dir(HEARTBEAT_PIN, 1);
     MCP23008_init(I2C_SDA, I2C_SCL);
     pin_io(7, PIN_OUTPUT);
     pin_io(0, PIN_INPUT);
+    prev_time = time_us_64();
     while (true) {
         int button = read_pin(0);
         if (button == 0) {
@@ -20,5 +29,15 @@ int main()
         else {
             set_pin(7, 0);
         }
+        if (time_us_64() - prev_time >= HEARTBEAT_INTERVAL) {
+            prev_time = time_us_64();
+            if (heartbeat_state == 0) {
+                heartbeat_state = 1;
+            }
+            else {
+                heartbeat_state = 0;
+            }
+        }
+        gpio_put(HEARTBEAT_PIN, heartbeat_state);
     }
 }
